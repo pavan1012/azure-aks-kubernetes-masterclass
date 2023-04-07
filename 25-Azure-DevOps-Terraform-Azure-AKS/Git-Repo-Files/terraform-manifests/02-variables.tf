8e7d37a3062d24c35a84f6d4b06603a1412c29ee
pavan1012 <48289006+pavan1012@users.noreply.github.com>
2023-04-07 11:56:47 +0530

    Update 02-variables.tf

--- 25-Azure-DevOps-Terraform-Azure-AKS/Git-Repo-Files/terraform-manifests/02-variables.tf
+++ 25-Azure-DevOps-Terraform-Azure-AKS/Git-Repo-Files/terraform-manifests/02-variables.tf
@@ -21,7 +21,7 @@ variable "resource_group_name" {
 variable "environment" {
   type = string  
   description = "This variable defines the Environment"  
-  #default = "dev2"
+  default = "dev2"
 }
 
 
@@ -29,21 +29,20 @@ variable "environment" {
 
 # SSH Public Key for Linux VMs
 variable "ssh_public_key" {
-  #default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
 
 # Windows Admin Username for k8s worker nodes
-variable "windows_admin_username" {
-  type = string
-  default = "azureuser"
-  description = "This variable defines the Windows admin username k8s Worker nodes"  
-}
+#variable "windows_admin_username" {
+#  type = string
+#  default = "azureuser"
+#  description = "This variable defines the Windows admin username k8s Worker nodes"  
+#}
 
 # Windows Admin Password for k8s worker nodes
-variable "windows_admin_password" {
-  type = string
-  default = "P@ssw0rd1234"
-  description = "This variable defines the Windows admin password k8s Worker nodes"  
-}
-
+#variable "windows_admin_password" {
+#  type = string
+#  default = "P@ssw0rd1234"
+#  description = "This variable defines the Windows admin password k8s Worker nodes"  
+#}
